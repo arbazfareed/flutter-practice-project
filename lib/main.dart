735214930b0908286377a08c3754a41636c952ca
arbazfareed <f201014@cfd.nu.edu.pk>
2023-09-28 05:01:10 -0700

    Stateless widget with container

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+MyApp()
+  );
+}
+// control space
+// stl and stf
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black38,
         appBar: AppBar(
@@ -10,9 +20,11 @@ void main() {
           centerTitle: true, // Center the title
           backgroundColor: Colors.deepOrange, // Set the background color
         ),
+
+        body: Container(
+          color:Colors.red,
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
-// control space
-// stl and stf
