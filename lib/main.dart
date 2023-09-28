443d645b176d4662baaf16e833f51f79e15686da
arbazfareed <f201014@cfd.nu.edu.pk>
2023-09-28 03:59:27 -0700

    Flutter Basic Work

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+runApp(
+  MaterialApp(
+    home:Scaffold(
+      appBar: AppBar(
+       centerTitle: true,
+
+      )
+    )
+  ),
+);
+}
+
+// control space
+// stl and stf
