92abc5dd0e5fca9cd1b66bd6f6891f645497138c
arbazfareed <f201014@cfd.nu.edu.pk>
2023-09-28 04:44:33 -0700

    Add  AppBar title name

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-runApp(
-  MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-       centerTitle: true,
-
-      )
-    )
-  ),
-);
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black38,
+        appBar: AppBar(
+          title: Text('My App'), // Add a title widget inside AppBar
+          centerTitle: true, // Center the title
+          backgroundColor: Colors.deepOrange, // Set the background color
+        ),
+      ),
+    ),
+  );
 }
-
 // control space
 // stl and stf
